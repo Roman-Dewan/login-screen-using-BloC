4b49e089990dc88bc20a5c197a311efe963e3a6b
Roman-Dewan <mdromandewan1888@gmail.com>
2026-02-07 00:06:29 +0600

    login page work done.

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -20,81 +20,71 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Center(
-          child: Form(
-            key: _formKey,
-            child: Column(
-            children: [
-              Image.asset('assets/images/signin_balls.png'),
-              const Text(
-                'Sign in.',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 50,
-                ),
+      body: BlocListener<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if(state is AuthFailure){
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text(state.error)),
+            );
+          }
+        },
+        child: SingleChildScrollView(
+          child: Center(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  Image.asset('assets/images/signin_balls.png'),
+                  const Text(
+                    'Sign in.',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 50,
+                    ),
+                  ),
+                  const SizedBox(height: 50),
+                  const SocialButton(
+                      iconPath: 'assets/svgs/g_logo.svg',
+                      label: 'Continue with Google'),
+                  const SizedBox(height: 20),
+                  const SocialButton(
+                    iconPath: 'assets/svgs/f_logo.svg',
+                    label: 'Continue with Facebook',
+                    horizontalPadding: 90,
+                  ),
+                  const SizedBox(height: 15),
+                  const Text(
+                    'or',
+                    style: TextStyle(
+                      fontSize: 17,
+                    ),
+                  ),
+                  const SizedBox(height: 15),
+                  LoginField(
+                    hintText: 'Email',
+                    controller: emailController,
+                  ),
+                  const SizedBox(height: 15),
+                  LoginField(
+                    hintText: 'Password',
+                    controller: passwordController,
+                  ),
+                  const SizedBox(height: 20),
+                  GradientButton(
+                    onPressed: () {
+                      context.read<AuthBloc>().add(
+                            AuthLoginRequested(
+                              email: emailController.text.trimLeft(),
+                              password: passwordController.text.trim(),
+                            ),
+                          );
+                    },
+                  ),
+                ],
               ),
-              const SizedBox(height: 50),
-              const SocialButton(
-                  iconPath: 'assets/svgs/g_logo.svg',
-                  label: 'Continue with Google'),
-              const SizedBox(height: 20),
-              const SocialButton(
-                iconPath: 'assets/svgs/f_logo.svg',
-                label: 'Continue with Facebook',
-                horizontalPadding: 90,
-              ),
-              const SizedBox(height: 15),
-              const Text(
-                'or',
-                style: TextStyle(
-                  fontSize: 17,
-                ),
-              ),
-              const SizedBox(height: 15),
-              LoginField(
-                hintText: 'Email',
-                controller: emailController,
-                // validator: (value) {
-                //   if (value == null || value.isEmpty) {
-                //     return 'Please enter your email';
-                //   }
-                //   if (!RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(value)) {
-                //     return 'Please enter a valid email';
-                //   }
-                //   return null;
-                // },
-              ),
-              const SizedBox(height: 15),
-              LoginField(
-                hintText: 'Password',
-                controller: passwordController,
-                // validator: (value) {
-                //   if (value == null || value.isEmpty) {
-                //     return 'Please enter your password';
-                //   }
-                //   return null;
-                // },
-              ),
-              const SizedBox(height: 20),
-              GradientButton(
-                onPressed: () {
-                  // if (_formKey.currentState!.validate()) {
-                  //   ScaffoldMessenger.of(context).showSnackBar(
-                  //     const SnackBar(content: Text('Processing Data')),
-                  //   );
-                  // }
-                  context.read<AuthBloc>().add(AuthLoginRequested(
-                    email: emailController.text.trimLeft(), 
-                    password: passwordController.text.trim(),
-                   ),
-                  );
-                },
-              ),
-             ],
+            ),
           ),
         ),
-      ),
       ),
     );
   }
